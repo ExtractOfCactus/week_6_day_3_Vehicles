58d0fa6b85a65a3e495af8ca0677d6db33d17b59
Glen Young <extractofcactus@icloud.com>
2017-06-29 07:26:07 +0100

    Tank class hasSpeed passed

---
+++ class_management/Tank.java
@@ -0,0 +1,12 @@
+package class_management;
+import behaviours.*;
+
+public class Tank extends Vehicle implements Driveable {
+  int speed;
+  String type;
+
+  public Tank(int speed, String type) {
+    super(speed);
+    this.type = type;
+  }
+}
---
+++ class_management_specs/TankTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import class_management.*;
+
+public class TankTest {
+  Tank tank1;
+  Tank tank2;
+
+  @Before
+  public void before() {
+    tank1 = new Tank(30, "Sherman");
+    tank2 = new Tank(40, "Tiger");
+  }
+  
+  @Test
+  public void tankHasSpeed() {
+    assertEquals(30, tank1.getSpeed());
+  }  
+}
